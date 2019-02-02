8ce887fdf2b202d099bcfefdfe343d1125f1c9fb
thomas van der biest <th.vanderbiest@gmail.com>
2019-02-02 11:39:41 +0100

    init

---
+++ src/main/java/com/kheops/euler/Problem.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kheops.euler;
+
+/**
+ *
+ * @author thomas
+ */
+public abstract class Problem {
+
+    public Problem() {
+    }
+    
+    public abstract void calcul();
+}
